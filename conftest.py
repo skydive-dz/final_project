9df66a101a2a623513f30fa94b67c6ae6ae71465
skydive-dz <sn7363@mail.ru>
2021-10-09 10:20:34 +0300

    Modified conftest

--- conftest.py
+++ conftest.py
@@ -13,20 +13,17 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
     user_language = request.config.getoption("language")
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
+import time
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    time.sleep(3)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
