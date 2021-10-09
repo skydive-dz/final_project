560687f2c47b853484f4864d6824d94213793ef7
skydive-dz <sn7363@mail.ru>
2021-10-09 22:36:28 +0300

    Добавил переходы между страницами

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -29,6 +31,8 @@ def test_guest_should_see_register_form(browser):
     page = LoginPage(browser, login_link)
     page.open()
     page.should_be_register_form()
+
+
 
 
     # pytest -v --tb=line --language=en test_main_page.py
